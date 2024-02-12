2e6592022e2f0d8c39d6a3bf430e2b14b4c0bf30
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-12 23:31:31 +0530

    Solve the greeting problem

---
+++ greeting.js
@@ -0,0 +1,8 @@
+// WAP to greet a person given their first and last name
+
+// console.log("Hello Abhi....")
+
+let firstName = "Abhishek";
+let lastName = "Yadav";
+
+console.log("Hello" + " " + firstName + " " + lastName);
