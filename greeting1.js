fe041bb5e8f878493a7561326632caa31d805533
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-12 23:58:44 +0530

    Solve seconf greeting problem

---
+++ greeting1.js
@@ -0,0 +1,20 @@
+// WAP that greets a person based on their gender.
+
+pName1 = "Abhishek Yadav"
+gender1 = "Male"
+pName2 = "Twinkle Upadhayay"
+gender2 = "Female"
+
+
+if(gender1 == "Male"){
+    console.log("Hello Mr." + " " + pName1);
+}
+else{
+    console.log("Hello Ms." + " " + pName2);
+}
+if(gender2 == "Female"){
+    console.log("Hello Ms." + " " + pName2);
+}
+else{
+    console.log("Hello Mr." + " " + pName1);
+}
