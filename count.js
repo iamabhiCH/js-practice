07a1872b1aace77b51efd08db37307c0a1427ee1
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-13 00:04:10 +0530

    Solve count problem

---
+++ count.js
@@ -0,0 +1,10 @@
+// WAP that counts from 0 - 1000 and prints it.
+
+let cnt = 0;
+
+for(let i = 0; i <= 1000; i++){
+    cnt += i;
+    console.log(i);
+}
+
+console.log("Count of 0 - 1000 is : "+ cnt);
