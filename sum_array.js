da027723f0984dcb7421f3c233871dfdec4390c4
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-16 01:01:00 +0530

    Find the sum of all elements in the array problem

---
+++ sum_array.js
@@ -0,0 +1,11 @@
+// WAP to find the sum of all elements in array
+
+let arr = [90,789,234,7890,1234,5678,5647,4492];
+
+let sum = 0;
+
+for(let i = 0; i<arr.length;i++){
+    sum += arr[i];
+}
+
+console.log("The sum of array is : "+sum);
