66a48e5d5517787e08d4a8f07cf748ca1fcbff7c
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-16 00:52:51 +0530

    Find element and print the index of element in array problem

---
+++ find_element.js
@@ -0,0 +1,19 @@
+// WAP to find the existing element in an array an print its index
+
+let arr = [12,34,56,78,90,123,445,678,901,4456];
+let fe = 678;
+let x = false;
+let tmp = 0;
+for(let i = 0; i<arr.length; i++){
+    if(arr[i] == fe){
+        x = true;
+        tmp = i;
+    }
+}
+
+if(x == true){
+    console.log("Your element is present in array at index "+tmp);
+}
+else{
+    console.log("Your element is not present in array.");
+}
