518b4446e906eb3fe8ad03264c8631207efe673a
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-19 09:36:23 +0530

    Create an object from two arrays problem solved

---
+++ arrToObj.js
@@ -0,0 +1,19 @@
+// WAP to create an object from two arrays.
+
+let arr1 = [1,2,3,4,5,6,7]
+let arr2 = ["Abhishek","Manish","Roshan","Akash S","Akash M","Abhinav","Nitya P"]
+
+function convertToObj(a,b){
+    if(a.length != b.length || a.length == 0 || b.length == 0){
+        return null;
+    }
+
+    let myObj = {}
+
+    a.forEach((i,j) =>
+    {
+        myObj[i] = b[j];
+    }); return myObj;
+}
+
+console.log(convertToObj(arr1,arr2));
