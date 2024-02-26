a5892498ee0d030b3ebe2874c522e2ab82d1e671
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-26 15:01:20 +0530

    Javascript program to Convert Byte Array to JSON

---
+++ arr_byte.js
@@ -0,0 +1,7 @@
+// WAP to Convert Byte Array to JSON.
+
+let arr = new Uint8Array([65,66,72,73,83,72,69,75]);
+let txtDecoder = new TextDecoder();
+let res = txtDecoder.decode(arr);
+
+console.log(res);
