ef15a53a6fb1ce0eed1a761b07aea7b79379c984
Abhishek Yadav <abhishekay442080@gmail.com>
2024-02-26 14:39:21 +0530

    Sort an Array using JavaScript when Array Elements has Different Data Types problem solved.

---
+++ arr-sort1.js
@@ -0,0 +1,8 @@
+// WAP to Sort an Array using JavaScript when Array Elements has Different Data Types.
+
+const arr = [7,true,"Abhishek",false,24,'Yadav','hahsabdab',29.0123,NaN];
+
+const arr1 = arr.map(item => String(item));
+arr1.sort();
+
+console.log(arr1);
