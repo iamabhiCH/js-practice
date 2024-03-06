1736a5825ef32b187e93815842cc823a65d4dd06
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-06 15:48:15 +0530

    JavaScript Program to Find the Most Frequently Occurring Element in an Array completed

--- arr_occr_ele.js
+++ arr_occr_ele.js
@@ -1,2 +1,16 @@
 // JavaScript Program to Find the Most Frequently Occurring Element in an Array
 
+let arr = [12,45,67,34,34,2,11,34];
+
+function mostOccrElement(arr){
+    const cnt = arr.reduce((ac, num) => {
+        ac[num] = (ac[num] || 0) + 1;
+        return ac;
+    }, {}); 
+
+    return Object.keys(cnt).reduce((a,b) =>
+        (cnt[a] > cnt[b] ? a : b)
+    );
+}
+
+console.log(mostOccrElement(arr));
