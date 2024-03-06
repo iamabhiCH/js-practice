6ab4347072ecae58334a6a549bad140e46c1637f
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-06 15:32:37 +0530

    Merge Two Arrays Without Creating a New Array completed

--- arr_merge1.js
+++ arr_merge1.js
@@ -1,1 +1,7 @@
 //WAP  Merge Two Arrays Without Creating a New Array
+
+let arr1 = [12,34,56,7,8,90,11];
+let arr2 = [3,5,77,80,10,72,29,16];
+
+Array.prototype.push.apply(arr1, arr2);
+console.log(arr1);
