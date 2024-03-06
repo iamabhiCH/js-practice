a59e6c2f8768436cf897344b2d752c4ee5ef4a33
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-06 15:40:30 +0530

    JavaScript Program for K-th Largest Sum Contiguous Subarray completed.

--- arr_sub-array.js
+++ arr_sub-array.js
@@ -1,2 +1,24 @@
 // JavaScript Program for K-th Largest Sum Contiguous Subarray
 
+let arr = [24,56,-190,70,25,13,9];
+let n = arr.length;
+let k = 7;
+
+function kthContSubarray(arr, n, k){
+    let res = [];
+    for(let i = 0; i < n; i++){
+        let s = 0;
+
+        for(let j = i; j < n; j++){
+            s += arr[j];
+            res.push(s);
+        }
+    }
+
+    res.sort();
+    res.reverse();
+
+    return res[k - 1];
+}
+
+console.log(kthContSubarray(arr,n,k));
