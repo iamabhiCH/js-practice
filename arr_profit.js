e263eb0edb1e27691022d5d1f62228748c7c62e9
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-07 18:59:49 +0530

    Find Maximum Profit by Buying and Selling a Share at Most Twice using Array completed

--- arr_profit.js
+++ arr_profit.js
@@ -1,1 +1,32 @@
-// WAP to Find Maximum Profit by Buying and Selling a Share at Most Twice using Array
+// WAP to Find Maximum prft by Buying and Selling a Share at Most Twice using Array
+
+let pr = [12,45,67,89,126,890];
+let x = pr.length;
+
+function maxprft(arr, n){
+    let prft = new Array(n);
+    for(let i = 0; i < n; i++){
+        prft[i] = 0;
+    }
+
+    let mxPrft = arr[ n - 1];
+    for(let i = n - 2; i >= 0; i--){
+        if(arr[i] > mxPrft){
+            mxPrft = arr[i];
+        }
+        prft[i] = Math.max(prft[i + 1], mxPrft - arr[i]);
+    }
+
+    let mnPrft = arr[0];
+    for(let i = 1; i<n; i++){
+        if(arr[i] < mnPrft){
+            mnPrft = arr[i];
+        }
+        prft[i] = Math.max(prft[i - 1],prft[i] + (arr[i] - mnPrft)); 
+    }
+
+    let res = prft[n - 1];
+    return res;
+}
+
+console.log("Maximum prft : "+ maxprft(pr,x));
