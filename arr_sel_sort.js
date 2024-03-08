9e52c438a099bf6bf029c39f7d87bdf033485eb4
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-09 01:24:46 +0530

    Selection sort in js completed.

--- arr_sel_sort.js
+++ arr_sel_sort.js
@@ -1,1 +1,27 @@
 // Javascript for selection sort
+
+const arr = [45,7,23,90,12,1,30];
+
+function swap(arr, x,y){
+    [arr[x],arr[y]] = [arr[y], arr[x]];
+}
+
+function selectionSort(arr){
+    let x = arr.length;
+    let mn;
+
+    for(let i = 0; i < x-1; i++){
+        mn = i;
+        for(let j = i+1; j < x; j++){
+            if(arr[j] < arr[mn]){
+                mn = j;
+            }
+        }
+        if(mn != 1)
+        swap(arr,i,mn);
+    }
+}
+
+console.log("Original array : " + arr);
+selectionSort(arr);
+console.log("Sorted Array : " + arr);
