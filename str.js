4b12d8d93e12dcfcca65250ed545ffe2a189cebc
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-11 13:42:30 +0530

    Get Character of Specific Position using JavaScript completed

--- str.js
+++ str.js
@@ -1,2 +1,7 @@
 // How to Get Character of Specific Position using JavaScript
 
+let str = "Hello Abhishek Yadav."
+let ind = 10;
+
+let str1 = str.charAt(ind);
+console.log("Charatcer at "+ind+"th position is : "+str1);
