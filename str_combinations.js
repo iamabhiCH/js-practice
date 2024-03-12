69c6118396e1b45b9425b99f30775545639455f4
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-12 22:36:07 +0530

    generate all combinations of a string in JavaScript completed.

--- str_combinations.js
+++ str_combinations.js
@@ -1,1 +1,17 @@
 // WAP to generate all combinations of a string in JavaScript.
+
+let cnt = 0;
+let possibleCombinations = (str) => {
+    let combinations = [];
+
+    for(let i = 0; i < str.length; i++){
+        for(let j = i + 1; j < str.length; j++){
+            combinations.push(str.slice(i,j));
+            cnt++;
+        }
+    }
+    return combinations;
+}
+
+console.log(possibleCombinations("Abhishek Yadav"));
+console.log("The total possible combinations of given string is : "+cnt);
