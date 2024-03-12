17de1b6c4e62eae7a222951dbc1c3e246e2d406d
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-12 22:12:31 +0530

    Replace multiple spaces with single space in JavaScript completed.

--- str_spaces.js
+++ str_spaces.js
@@ -1,1 +1,7 @@
 // WAP to replace multiple spaces with single space in JavaScript.
+
+let str1 = "Hello,    I am    Abhishek         Yadav";
+console.log(str1);
+
+let str = str1.replace(/\s+/g, ' ');
+console.log(str);
