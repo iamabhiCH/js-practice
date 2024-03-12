9ab6a2852816d747e970bfe3f0e990d842321d72
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-12 22:24:30 +0530

    Checking the quality of the two strings completed.

--- str_equal.js
+++ str_equal.js
@@ -1,2 +1,11 @@
 // WAP to check that the both strings are equal or not.
 
+const str1 = "Abhishek Yadav";
+const str2 = "Abhishek Yadav";
+
+if(str1 === str2){
+    console.log("Both strings are equal.");
+}
+else{
+    console.log("Both strings are not equal.");
+}
