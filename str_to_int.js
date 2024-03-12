209c6f0a38fe2a37a7cda04eb2d5bbd87618fac6
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-12 22:29:37 +0530

    convert a string into a integer without using parseInt() function in JavaScript completed.

--- str_to_int.js
+++ str_to_int.js
@@ -1,1 +1,13 @@
 // WAP to convert a string into a integer without using parseInt() function in JavaScript.
+
+function convertInt(){
+    let a ="100";
+    let b = a * 1;
+    console.log(b);
+    console.log(typeof(b));
+    let d = " 34 67 900" * 1;
+    console.log(d);
+    console.log(typeof(d));
+}
+
+convertInt();
