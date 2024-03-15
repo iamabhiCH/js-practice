c8150a0008e9a8c000cbc8314e103e60282eed9b
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-15 12:55:54 +0530

    find substring in a string completed.

--- str_substr.js
+++ str_substr.js
@@ -1,2 +1,20 @@
 // WAP to find substring of a string
 
+const str = "Hello, I am abhishek yadav";
+
+// first method
+console.log(str.substring(0, 4));
+
+// second method
+console.log(str.substr(0, 15));
+
+// third method
+console.log(str.slice(0, 13));
+
+// fourth method
+
+let fakeStr = "";
+for(let i = 0; i < 16; i++){
+    fakeStr += str[i];
+}
+console.log(fakeStr);
