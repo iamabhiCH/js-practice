476c61980e1600bb949c7c885659996341c43e31
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-15 12:44:35 +0530

    find unique characters of a string in JavaScript completed

--- str_unq_char.js
+++ str_unq_char.js
@@ -1,2 +1,19 @@
 // WAP to find unique characters of a string in JavaScript.
 
+let str1 = "Abhishek Yadav";
+let str2 = "iamabhiCH";
+
+function findUnique(str) {
+    let uniq = "";
+ 
+    for (let i = 0; i < str.length; i++) {
+        if (uniq.includes(str[i]) === false) {
+            uniq += str[i]
+        }
+    }
+    return uniq;
+}
+
+console.log(findUnique(str1));
+console.log(findUnique(str2));
+console.log(findUnique("ka haal baa"));
