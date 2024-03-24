1b5f7ce932b1644fa89e57b159aa353eeae4cbc2
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-24 12:43:33 +0530

    How to add method to String class in JavaScript.

---
+++ str_length.js
@@ -0,0 +1,9 @@
+// How to add method to String class in JavaScript 
+
+let str1 = "savukbhsdsha sch dshvbsdhvbasd hs bhsdvbdhb ";
+let str2 = "shsdvhsdkahdohuiyrgwiufhw hgewfgruibcbsklksdcnkj ksdjvbsjskjdfdubdjkbavksd";
+
+let l1 = str1.length;
+let l2 = str2.length;
+
+console.log(l1+l2);
