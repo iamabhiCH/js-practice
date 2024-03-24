b90cde3095f4aeb1e1f0ad5e5a216a34dc8d1e5e
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-24 11:32:41 +0530

    create half of the string in uppercase and the other half in lowercase completed

--- str_Uppercase.js
+++ str_Uppercase.js
@@ -1,2 +1,17 @@
 // WAP to create half of the string in uppercase and the other half in lowercase.
 
+let str = "abhishekyadav";
+let len = str.length;
+
+let firstHalfStr = '';
+let secondHalfStr = '';
+
+for(let i = 0; i < Math.ceil(len/2); i++) {
+    firstHalfStr += str[i];
+}
+for(let i = Math.ceil(len / 2); i < len; i++) {
+    secondHalfStr += str[i];
+}
+
+let newStr = firstHalfStr.toLocaleUpperCase() + secondHalfStr.toLowerCase();
+console.log("New String: ",newStr);
