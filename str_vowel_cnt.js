33444f48327423fb25b3eff868c7d9e0f6fed8ae
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-24 12:06:36 +0530

    get a number of vowels in a string in JavaScript completed.

--- str_vowel_cnt.js
+++ str_vowel_cnt.js
@@ -1,2 +1,17 @@
 // WAP to get a number of vowels in a string in JavaScript.
 
+let str = "abhishekYAdavabhiABCDIamABHI";
+
+function countVowels(str){
+    let v = "aeiouAEIOU";
+    let cnt  = 0;
+
+    for(let i = 0; i < str.length;  i++){
+        if(v.indexOf(str[i]) != -1){
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+console.log("The number of vowels in "+str+" is: "+countVowels(str));
