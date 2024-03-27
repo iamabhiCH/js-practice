c532a52755f1d2e7e15c55335f3dcf5c9639738b
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-27 08:58:10 +0530

    Remove a Character From String in JavaScript completed

--- str_remove_char.js
+++ str_remove_char.js
@@ -1,1 +1,9 @@
 // WAP to Remove a Character From String in JavaScript
+
+function removeCharFromString(){
+    let str = "sh sbjhs h cj dgvvfjbg";
+    str1 = str.replace("s","");
+    console.log(str1);
+}
+
+removeCharFromString();
