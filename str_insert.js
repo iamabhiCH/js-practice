971cab3b1439bc8e93ca94ff5523b19c5aeb51c5
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-27 08:48:35 +0530

    insert a string at a specific index in JavaScript

---
+++ str_insert.js
@@ -0,0 +1,9 @@
+// WAP to insert a string at a specific index in JavaScript;
+
+let str = "Abhishek Yadav";
+
+let str1 = " - ( iamabhi )";
+let i = 14;
+
+str2 = str.slice(0, i) + str1;
+console.log("New String : ", str2);
