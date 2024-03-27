b952583eb837e195013472fc86305b130c29090b
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-27 08:54:40 +0530

    How to create hash from string in JavaScript completed

--- str_to_hash.js
+++ str_to_hash.js
@@ -1,1 +1,18 @@
 //  How to create hash from string in JavaScript
+
+let str1 = "Abhishek Yadav";
+
+function strToHash(str){
+    let hs = 0;
+
+    if(str.length == 0) return hs;
+
+    for(let i = 0; i < str.length; i++){
+        ch = str.charCodeAt(i);
+        hs = ((hs << 5) - hs) + ch;
+        hs = hs & hs;
+    }
+    return hs;
+}
+
+console.log(strToHash(str1));
