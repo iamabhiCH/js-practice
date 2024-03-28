196ba2e5ab5a4d70936769b293e1d74bf33a5eeb
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-28 13:52:44 +0530

    remove text from a string in js completed

--- str_remove_text.js
+++ str_remove_text.js
@@ -1,1 +1,12 @@
 // WAP to remove text from a string in JavaScript
+
+let str = "bskjbsvjhsvsknvbsdh xczvgsdhvbhsdvbjhsvhjshvsdhvz hjsvjhvhcvjhvvsdj";
+
+function removeText(str){
+    console.log("Original String  : "+str);
+    
+    let str1 = str.replace("xczvgsdhvbhsdvbjhsvhjshvsdhvz", "");
+    return str1;
+}
+
+console.log("New String : "+removeText(str));
