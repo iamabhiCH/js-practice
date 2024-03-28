2eba7c1a52e296869728ce388a2836c7b36a8900
Abhishek Yadav <abhishekay442080@gmail.com>
2024-03-28 13:45:26 +0530

    convert string into float in js completed

--- str_to_float.js
+++ str_to_float.js
@@ -1,1 +1,11 @@
 // WAP to convert string into float in JavaScript
+
+let num = "12.5";
+
+function convert_to_float(n) {
+    let fv = +n;
+    return fv;
+}
+
+let x = convert_to_float(num);
+console.log(x);
