027d8df1f59b774de88c408f19085637a1437b58
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-01 11:25:09 +0530

    Number pattern problem solved.

--- pattern_number1.js
+++ pattern_number1.js
@@ -7,3 +7,22 @@
     1 2 3 4 5 6 7 8 9  
 */
 
+const prompt = require('prompt-sync')();
+
+const num = parseInt(prompt("Enter the line of the pattern : "));
+
+for(let i = 1; i < num; i++){
+    let arr = [];
+    let cnt = 1;
+
+    for(let j = 1; j < 2 * num; ++j){
+        if(i + j >= num + 1 && (i >= j - num + 1)){
+            arr.push(cnt);
+            cnt++;
+        }
+        else{
+            arr.push(' ');
+        }
+    }
+    console.log(arr.join(' '));
+}
