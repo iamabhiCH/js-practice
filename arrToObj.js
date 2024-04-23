0fcd630d29f70bcfcf21188ed508b154b1af7981
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-04-23 20:17:05 +0530

    Update arrToObj.js 23 4 24

--- arrToObj.js
+++ arrToObj.js
@@ -1,7 +1,7 @@
 // WAP to create an object from two arrays.
 
 let arr1 = [1,2,3,4,5,6,7]
-let arr2 = ["Abhishek","Manish","Roshan","Akash S","Akash M","Abhinav","Nitya P"]
+let arr2 = ["Abhishek","Manish","Roshan","Akash S","Akash M","Abhinav","Chandan Tiwari"]
 
 function convertToObj(a,b){
     if(a.length != b.length || a.length == 0 || b.length == 0){
