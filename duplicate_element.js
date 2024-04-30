456af57c1a6caea572063cbe6565f65515af7c4b
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-04-30 19:51:29 +0530

    Update duplicate_element.js 30 04 24

--- duplicate_element.js
+++ duplicate_element.js
@@ -1,6 +1,6 @@
 // WAP to remove the duplicate element in array.
 
-let arr = [12,34,245,45,78,90,12,4,56,34,78,90,89,9,7,4,24];
+let arr = [12,34,245,45,78,90,12,4,56,34,78,90,89,9,7,4,56,4,12,90,24];
 
 let arr1 = [...new Set(arr)];
 
