cf30c9ee7a8a610a184a2961ab41c6510bcbe143
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-05-02 13:25:40 +0530

    Update arrToStr.js 2

--- arrToStr.js
+++ arrToStr.js
@@ -1,6 +1,6 @@
 // WAP to create a string by joining the elements of an array.
 
-let arr = ["Hello","I", "am","abhi."];
+let arr = ["Hello","I", "am","Abhishek."];
 let str = arr.toString(arr);
 
 console.log(str);
