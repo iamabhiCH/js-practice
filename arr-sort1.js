3616cf8fa0bcab4f9d550dab07956a49695d395d
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-05-13 10:14:43 +0530

    Update arr-sort1.js 13 05 24

--- arr-sort1.js
+++ arr-sort1.js
@@ -1,6 +1,6 @@
 // WAP to Sort an Array using JavaScript when Array Elements has Different Data Types.
 
-const arr = [7,true,"Abhishek",false,24,'Yadav','hahsabdab',29.0123,NaN];
+const arr = [7,true,"Abhishek",false,24,'Yadav','hahsabdab',29.0123,NaN,"iamabhi.07"];
 
 const arr1 = arr.map(item => String(item));
 arr1.sort();
