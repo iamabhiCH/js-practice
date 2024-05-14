a98a1b9a59a5f7b088381e5581a050df9e6f3974
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-05-14 20:37:31 +0530

    Update arr_byte.js 14 05

--- arr_byte.js
+++ arr_byte.js
@@ -1,6 +1,6 @@
 // WAP to Convert Byte Array to JSON.
 
-let arr = new Uint8Array([65,66,72,73,83,72,69,75]);
+let arr = new Uint8Array([65,66,72,73,83,72,69,75,689,342,2407]);
 let txtDecoder = new TextDecoder();
 let res = txtDecoder.decode(arr);
 
