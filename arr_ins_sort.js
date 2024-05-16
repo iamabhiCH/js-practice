cf01684a95754fcb361e1cdfa63cbadf52703636
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-05-16 14:50:54 +0530

    Update arr_ins_sort.js 16 05

--- arr_ins_sort.js
+++ arr_ins_sort.js
@@ -1,23 +1,23 @@
 // Javascript for insertion  sort
 
-const arr = [67,12,45,7,30,80,56,1,22];
+const arr = [67,12,45,7,30,80,56,1,22,59];
 
 function insertionSort(arr){
     let n = arr.length;
-    let k;
+    let ab;
 
     let i,j;
     for(i = 0; i < n; i++){
-        k = arr[i];
+        ab = arr[i];
         j = i - 1;
-        for(j; j >= 0, arr[j]>k; --j){
+        for(j; j >= 0, arr[j]>ab; --j){
             arr[j+1] = arr[j];
         }
-        arr[j+1] = k;
+        arr[j+1] = ab;
     }
 }
 
 
-console.log("Original Array : ",arr);
+console.log("Original Array is : ",arr);
 insertionSort(arr);
-console.log("Selection Array : ",arr);
+console.log("Selection Array is : ",arr);
