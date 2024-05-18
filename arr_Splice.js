04e37238c67d848a87763af32a4b98e75a93aa61
Abhishek Yadav <105651140+iamabhiCH@users.noreply.github.com>
2024-05-18 15:14:59 +0530

    Update arr_Splice.js 18 05 24

--- arr_Splice.js
+++ arr_Splice.js
@@ -1,8 +1,8 @@
 // WAP to splice an array without mutating the original Array.
 // By the help of splice method we extract the elements of array.
 
-let myArr = ["I", "Me", "Mine", "Mera", "Hamara", "Hum"]
+let myArr = ["I", "Me", "Mine", "Mera", "Hamara", "Hum","tum","tumhara",]
 
-let extractArr = myArr.splice(4,6);
+let extractArr = myArr.splice(4,8);
 console.log(extractArr);
 console.log(myArr)
